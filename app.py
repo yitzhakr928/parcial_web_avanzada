8d5235a61d3ef096ee5e1294be7edd29f19e43b2
Yitzhak928 <yitzhakruiz@hotmail.com>
2022-11-19 21:35:48 -0500

    front-end listo

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ app= Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/registro/')
+def registro():
+    return render_template('registro.html')
+
+
+@app.route('/contacto/')
+def contacto():
+    return render_template('Contacto.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
